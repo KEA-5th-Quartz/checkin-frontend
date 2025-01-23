b96182998ab8545eb4290167c6af2f43fdca8cbd
kimgorok <kjyse2@naver.com>
2025-01-23 18:17:16 +0900

    ✨feat: [사용자] 요청 티켓이 없을 때의 화면 추가 Related to: #7

--- src/components/user/UserBoard.vue
+++ src/components/user/UserBoard.vue
@@ -5,6 +5,9 @@ import StatusBadge from '../common/Badges/StatusBadge.vue';
 import { tableDataTest } from '../manager/dashboardTest';
 import UserTicket from './UserTicket.vue';
 import { useUserTicketListStore } from '@/stores/userTicketListStore';
+import SvgIcon from '../common/SvgIcon.vue';
+import { CreateTicketIcon } from '@/assets/icons/path';
+import UserSearchFilter from './UserSearchFilter.vue';
 
 const ticketStore = useUserTicketListStore();
 
@@ -35,90 +38,106 @@ const handleCheckboxClick = (event: Event, id: number) => {
 </script>
 
 <template>
-  <section class="overflow-x-auto mt-5 px-5 pb-20">
-    <div class="min-h-[calc(100vh-300px)] h-full">
-      <table class="min-w-full table-fixed">
-        <thead class="manager-thead">
-          <tr>
-            <th v-if="ticketStore.isDeleteMode" class="manager-th w-[1%]">선택</th>
-            <th :class="['manager-th w-[5%]', ticketStore.isDeleteMode ? 'pl-0' : 'pl-6']">번호</th>
-            <th class="manager-th text-start w-[25%]">제목</th>
-            <th class="manager-th w-[10%]">1차 <span class="hidden lg:inline-block">카테고리</span></th>
-            <th class="manager-th w-[7.5%]">2차 <span class="hidden lg:inline-block">카테고리</span></th>
-            <th class="manager-th w-[25%]">설명</th>
-            <th class="manager-th w-[7.5%]">진행 상태</th>
-            <th class="manager-th w-[10%]">담당자</th>
-            <th class="manager-th w-[5%]">마감일</th>
-            <th class="manager-th w-[5%] pr-6">중요도</th>
-          </tr>
-        </thead>
+  <section v-if="tableDataTest.length !== 0">
+    <UserSearchFilter />
+    <article class="overflow-x-auto mt-5 px-5 pb-20">
+      <div class="min-h-[calc(100vh-300px)] h-full">
+        <table class="min-w-full table-fixed">
+          <thead class="manager-thead">
+            <tr>
+              <th v-if="ticketStore.isDeleteMode" class="manager-th w-[1%]">선택</th>
+              <th :class="['manager-th w-[5%]', ticketStore.isDeleteMode ? 'pl-0' : 'pl-6']">번호</th>
+              <th class="manager-th text-start w-[25%]">제목</th>
+              <th class="manager-th w-[10%]">1차 <span class="hidden lg:inline-block">카테고리</span></th>
+              <th class="manager-th w-[7.5%]">2차 <span class="hidden lg:inline-block">카테고리</span></th>
+              <th class="manager-th w-[25%]">설명</th>
+              <th class="manager-th w-[7.5%]">진행 상태</th>
+              <th class="manager-th w-[10%]">담당자</th>
+              <th class="manager-th w-[5%]">마감일</th>
+              <th class="manager-th w-[5%] pr-6">중요도</th>
+            </tr>
+          </thead>
 
-        <tbody class="whitespace-nowrap">
-          <tr
-            v-for="item in tableDataTest"
-            :key="item.id"
-            class="hover:bg-white-1 relative"
-            @click="handleRowClick(item.id)"
-          >
-            <td v-if="ticketStore.isDeleteMode" class="manager-td">
-              <div class="flex items-center justify-center">
-                <input
-                  type="checkbox"
-                  :class="[item.status !== '진행중' ? 'w-4 h-4 cursor-pointer' : 'hidden']"
-                  :checked="ticketStore.selectedTickets.has(item.id)"
-                  @click="(e) => handleCheckboxClick(e, item.id)"
-                />
-              </div>
-            </td>
-            <td :class="['manager-td max-w-0', ticketStore.isDeleteMode ? 'pl-0' : 'pl-6']">
-              <p :title="item.id as unknown as string">
-                {{ item.id }}
-              </p>
-            </td>
-            <td class="manager-td max-w-0 text-start">
-              <p class="truncate" :title="item.title">
-                {{ item.title }}
-              </p>
-            </td>
-            <td class="manager-td max-w-0">
-              <p class="truncate">
-                {{ item.category1 }}
-              </p>
-            </td>
-            <td class="manager-td max-w-0">
-              <p class="truncate">
-                {{ item.category2 }}
-              </p>
-            </td>
-            <td class="manager-td max-w-0 text-start">
-              <p class="truncate" :title="item.description">
-                {{ item.description }}
-              </p>
-            </td>
-            <td class="manager-td text-center">
-              <StatusBadge :status="item.status" size="md" />
-            </td>
-            <td class="manager-td">
-              <div class="flex items-center gap-2 ml-4">
-                <div class="h-7 w-7 rounded-full bg-pink-200" />
-                <span class="truncate">{{ item.assignee }}</span>
-              </div>
-            </td>
-            <td class="manager-td">
-              <p class="truncate">
-                {{ item.dueDate }}
-              </p>
-            </td>
-            <td class="manager-td pr-6">
-              <PriorityBadge :priority="item.priority" size="md" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+          <tbody class="whitespace-nowrap">
+            <tr
+              v-for="item in tableDataTest"
+              :key="item.id"
+              class="hover:bg-white-1 relative"
+              @click="handleRowClick(item.id)"
+            >
+              <td v-if="ticketStore.isDeleteMode" class="manager-td">
+                <div class="flex items-center justify-center">
+                  <input
+                    type="checkbox"
+                    :class="[item.status !== '진행중' ? 'w-4 h-4 cursor-pointer' : 'hidden']"
+                    :checked="ticketStore.selectedTickets.has(item.id)"
+                    @click="(e) => handleCheckboxClick(e, item.id)"
+                  />
+                </div>
+              </td>
+              <td :class="['manager-td max-w-0', ticketStore.isDeleteMode ? 'pl-0' : 'pl-6']">
+                <p :title="item.id as unknown as string">
+                  {{ item.id }}
+                </p>
+              </td>
+              <td class="manager-td max-w-0 text-start">
+                <p class="truncate" :title="item.title">
+                  {{ item.title }}
+                </p>
+              </td>
+              <td class="manager-td max-w-0">
+                <p class="truncate">
+                  {{ item.category1 }}
+                </p>
+              </td>
+              <td class="manager-td max-w-0">
+                <p class="truncate">
+                  {{ item.category2 }}
+                </p>
+              </td>
+              <td class="manager-td max-w-0 text-start">
+                <p class="truncate" :title="item.description">
+                  {{ item.description }}
+                </p>
+              </td>
+              <td class="manager-td text-center">
+                <StatusBadge :status="item.status" size="md" />
+              </td>
+              <td class="manager-td">
+                <div class="flex items-center gap-2 ml-4">
+                  <div class="h-7 w-7 rounded-full bg-pink-200" />
+                  <span class="truncate">{{ item.assignee }}</span>
+                </div>
+              </td>
+              <td class="manager-td">
+                <p class="truncate">
+                  {{ item.dueDate }}
+                </p>
+              </td>
+              <td class="manager-td pr-6">
+                <PriorityBadge :priority="item.priority" size="md" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <UserTicket v-if="selectedTicketId" :ticket-id="selectedTicketId" @close="handleCloseModal" />
+    </article>
+  </section>
+  <section v-else class="w-full flex flex-col items-center pb-40">
+    <div class="flex flex-col items-center gap-8 mt-32">
+      <img src="@/assets/no-ticket.png" />
+      <div class="flex flex-col items-center gap-2">
+        <h1 class="text-xl">아직 요청한 티켓이 없어요</h1>
+        <p>티켓 생성을 눌러 새로운 티켓을 생성하세요</p>
+      </div>
     </div>
 
-    <div>페이지네이션</div>
+    <button class="flex items-center bg-primary-0 py-2.5 px-8 rounded text-white-0 gap-2.5 text-sm font-semibold mt-20">
+      <SvgIcon :icon="CreateTicketIcon" />
 
-    <UserTicket v-if="selectedTicketId" :ticket-id="selectedTicketId" @close="handleCloseModal" />
+      티켓 생성
+    </button>
   </section>
 </template>
