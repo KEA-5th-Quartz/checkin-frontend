8ff178b4c6e4d8334a66d32a8cca9a89a8fc501c
Pseokjong <seogjongb@gamil.com>
2025-01-23 14:39:56 +0900

    ✨feat: [관리자] 카테고리 수정, 삭제 드롭다운 메뉴 기능 추가 Related to: #2

--- src/components/Administrator/CategoryManagement/CategoryList.vue
+++ src/components/Administrator/CategoryManagement/CategoryList.vue
@@ -7,13 +7,11 @@
         <li
           v-for="category in primaryCategories"
           :key="category.id"
+          class="flex justify-between items-center p-3 border rounded-md cursor-pointer hover:bg-gray-3"
           @click="selectPrimaryCategory(category)"
-          :class="[
-            'p-3 border rounded-md cursor-pointer',
-            selectedPrimaryCategory?.id === category.id ? 'bg-primary-3 text-white-0' : 'hover:bg-gray-3',
-          ]"
         >
-          {{ category.name }}
+          <span>{{ category.name }}</span>
+          <CategoryDropdown />
         </li>
       </ul>
     </div>
@@ -25,9 +23,10 @@
         <li
           v-for="category in filteredSecondaryCategories"
           :key="category.id"
-          class="p-3 border rounded-md cursor-pointer hover:bg-gray-3"
+          class="flex justify-between items-center p-3 border rounded-md cursor-pointer hover:bg-gray-3"
         >
-          {{ category.name }}
+          <span>{{ category.name }}</span>
+          <CategoryDropdown />
         </li>
       </ul>
       <p v-else class="text-gray-0">해당 2차 카테고리가 없습니다.</p>
@@ -38,6 +37,7 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import { primaryCategories, SecondaryCategories } from '../CategoryTest';
+import CategoryDropdown from './CategoryDropdown.vue';
 
 // 선택된 1차 카테고리 상태
 const selectedPrimaryCategory = ref(null);
@@ -51,5 +51,15 @@ const filteredSecondaryCategories = computed(() => {
 // 1차 카테고리 선택 핸들러
 function selectPrimaryCategory(category) {
   selectedPrimaryCategory.value = category;
+}
+
+// 수정 이벤트 핸들러
+function handleEdit(category) {
+  console.log('수정할 카테고리:', category);
+}
+
+// 삭제 이벤트 핸들러
+function handleDelete(category) {
+  console.log('삭제할 카테고리:', category);
 }
 </script>
