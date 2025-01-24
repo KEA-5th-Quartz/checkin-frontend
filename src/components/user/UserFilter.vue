b4dbf9d5579582e1e6bb054ba6fa9d308c8b2033
kimgorok <kjyse2@naver.com>
2025-01-24 21:07:40 +0900

    🐛fix: [사용자] 필터 드롭다운 onClickOutside 의도대로 안된 문제 해결 Related to: #7

--- src/components/user/UserFilter.vue
+++ src/components/user/UserFilter.vue
@@ -4,30 +4,20 @@ import StatusBadge from '@/components/common/Badges/StatusBadge.vue';
 import SvgIcon from '@/components/common/SvgIcon.vue';
 import { onClickOutside } from '@vueuse/core';
 import { ref } from 'vue';
-import { firstCategory, managerOptions, status } from '../manager/ticketOptionTest';
+import { firstCategory, status } from '../manager/ticketOptionTest';
 
 const emit = defineEmits(['closeFilter']);
 const modalRef = ref<HTMLElement | null>(null);
+const categoryRef = ref<HTMLElement | null>(null);
 
 // 필터 선택 상태 관리
-const selectedManagers = ref<string[]>([]);
 const selectedStatuses = ref<string[]>([]);
 const selectedCategories = ref<string[]>([]);
 
 // 드롭다운 상태 관리
-const isManagerDropdownOpen = ref(false);
 const isCategoryDropdownOpen = ref(false);
 
 // 필터 토글 함수들
-
-const toggleManager = (value: string) => {
-  if (selectedManagers.value.includes(value)) {
-    selectedManagers.value = selectedManagers.value.filter((item) => item !== value);
-  } else {
-    selectedManagers.value.push(value);
-  }
-};
-
 const toggleStatus = (status: string) => {
   if (selectedStatuses.value.includes(status)) {
     selectedStatuses.value = selectedStatuses.value.filter((s) => s !== status);
@@ -46,10 +36,13 @@ const toggleCategory = (category: string) => {
 
 // 저장 버튼 클릭 핸들러
 const handleSave = () => {
-  console.log('담당자', selectedManagers.value);
   console.log('상태:', selectedStatuses.value);
   console.log('카테고리:', selectedCategories.value);
 };
+
+onClickOutside(categoryRef, () => {
+  isCategoryDropdownOpen.value = false;
+});
 
 onClickOutside(modalRef, () => {
   emit('closeFilter');
@@ -62,46 +55,17 @@ onClickOutside(modalRef, () => {
     class="bg-white-0 rounded-lg w-[300px] max-h-[600px] overflow-y-auto shadow-md pb-4 hide-scrollbar z-20"
   >
     <div class="py-3 px-8 bg-primary-0 text-white-1 font-bold">필터</div>
-    <!-- 담당자 -->
-    <section class="flex flex-col px-7 py-5">
-      <p class="border-b-2 border-gray-0 max-w-fit pr-4 mb-2">담당자</p>
-      <div class="relative">
-        <div
-          @click="isManagerDropdownOpen = !isManagerDropdownOpen"
-          class="flex items-center justify-between border border-gray-2 hover:border-gray-1 rounded-[10px] py-[5px] text-start px-3 cursor-pointer"
-        >
-          <p v-if="selectedManagers.length === 0" class="opacity-30">담당자 선택</p>
-          <p v-else>{{ selectedManagers.length }}명 선택</p>
-          <SvgIcon
-            :icon="ArrowDownIcon"
-            :class="['transition-transform duration-200', isManagerDropdownOpen ? 'rotate-180' : '']"
-          />
-        </div>
 
-        <div
-          v-if="isManagerDropdownOpen"
-          class="absolute z-20 top-9 mt-1 w-full bg-white-0 border border-gray-2 rounded-lg shadow-md max-h-60 overflow-y-auto hide-scrollbar"
-        >
-          <div
-            v-for="manager in managerOptions"
-            :key="manager.id"
-            @click="toggleManager(manager.value)"
-            class="px-3 py-2 hover:bg-gray-3 cursor-pointer flex items-center justify-between"
-            :class="[
-              selectedManagers.includes(manager.value) ? 'bg-gray-3 text-gray-0 hover:font-semibold' : 'text-gray-2',
-            ]"
-          >
-            <p>{{ manager.label }}</p>
-          </div>
-        </div>
-      </div>
-    </section>
     <!-- 카테고리 -->
-    <section class="flex flex-col px-7 pb-5">
+    <section class="flex flex-col px-7 py-5">
       <p class="border-b-2 border-gray-0 max-w-fit mb-3.5">카테고리</p>
-      <div class="relative">
+      <div class="relative" ref="categoryRef">
         <div
-          @click="isCategoryDropdownOpen = !isCategoryDropdownOpen"
+          @click="
+            console.log(isCategoryDropdownOpen);
+            isCategoryDropdownOpen = !isCategoryDropdownOpen;
+            console.log(isCategoryDropdownOpen);
+          "
           class="flex items-center justify-between border border-gray-2 hover:border-gray-1 rounded-[10px] py-[5px] text-start px-3 cursor-pointer"
         >
           <p v-if="selectedCategories.length === 0" class="opacity-30">카테고리 선택</p>
