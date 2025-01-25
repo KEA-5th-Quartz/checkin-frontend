e689bee7cf6e8d6bef930102f64322e43933428f
kimgorok <kjyse2@naver.com>
2025-01-26 01:25:32 +0900

    ✨feat: 사이드바 페이지별 조건부 렌더링 적용 Related to: #13

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,44 +1,58 @@
+import BlankLayout from '@/views/layouts/BlankLayout.vue';
+import SidebarLayout from '@/views/layouts/SidebarLayout.vue';
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
-  // 공통
   {
     path: '/',
-    name: 'home',
-    component: () => import('../views/HomeView.vue'),
+    component: BlankLayout,
+    children: [
+      {
+        path: '/',
+        name: 'login',
+        component: () => import('../views/LoginView.vue'),
+      },
+    ],
   },
   {
-    path: '/settings',
-    name: 'settings',
-    component: () => import('../views/HomeView.vue'),
-  },
-  // 담당자
-  {
-    path: '/manager/dashboard',
-    name: 'manager-dashboard',
-    component: () => import('../views/manager/DashboardView.vue'),
-  },
-  {
-    path: '/manager/statistics',
-    name: 'manager-statistics',
-    component: () => import('../views/manager/ManagerStatisticsView.vue'),
-  },
-  // 관리자
-  {
-    path: '/administrator/memberManagement',
-    name: 'MemberManagement',
-    component: () => import('../views/Administrator/MemberManagement.vue'),
-  },
-  {
-    path: '/administrator/CategoryManagement',
-    name: 'CategoryManagement',
-    component: () => import('../views/Administrator/CategoryManagement.vue'),
-  },
-  // 사용자
-  {
-    path: '/user/tickets',
-    name: 'user-tickets',
-    component: () => import('../views/user/TicketView.vue'),
+    path: '/',
+    component: SidebarLayout,
+    children: [
+      // 공통
+      {
+        path: 'settings',
+        name: 'settings',
+        component: () => import('../views/LoginView.vue'),
+      },
+      // 담당자
+      {
+        path: 'manager/dashboard',
+        name: 'manager-dashboard',
+        component: () => import('../views/manager/DashboardView.vue'),
+      },
+      {
+        path: 'manager/statistics',
+        name: 'manager-statistics',
+        component: () => import('../views/manager/ManagerStatisticsView.vue'),
+      },
+      // 관리자
+      {
+        path: 'administrator/memberManagement',
+        name: 'MemberManagement',
+        component: () => import('../views/Administrator/MemberManagement.vue'),
+      },
+      {
+        path: 'administrator/CategoryManagement',
+        name: 'CategoryManagement',
+        component: () => import('../views/Administrator/CategoryManagement.vue'),
+      },
+      // 사용자
+      {
+        path: 'user/tickets',
+        name: 'user-tickets',
+        component: () => import('../views/user/TicketView.vue'),
+      },
+    ],
   },
 ];
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,6 @@
 
 <template>
   <div class="home">
-    <p class="text-xl font-extrabold">안녕</p>
+    <p class="text-xl font-extrabold">로그인</p>
   </div>
 </template>
