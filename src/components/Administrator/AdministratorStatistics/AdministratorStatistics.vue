831e6e445d6189ba83c059c1d826cab3637424e3
Pseokjong <seogjongb@gamil.com>
2025-01-26 22:21:40 +0900

    ✨feat: [관리자] 관리자 통계 페이지 카테고리별 티켓 수 레이아웃 제작 Related to: #15

--- src/components/Administrator/AdministratorStatistics/AdministratorStatistics.vue
+++ src/components/Administrator/AdministratorStatistics/AdministratorStatistics.vue
@@ -1,8 +1,38 @@
+<template>
+  <section class="overflow-x-auto mt-5 p-5 pb-20">
+    <!-- 담당자별 티켓 진행 상황 -->
+    <div class="p-5 bg-white-0">
+      <h2 class="font-bold mb-5">담당자별 티켓 진행 현황</h2>
+      <div class="flex gap-4">
+        <button
+          v-for="filter in ['WEEK', 'MONTH', 'QUARTER']"
+          :key="filter"
+          :class="[
+            'px-4 py-2 rounded-lg border text-sm',
+            timeFilter === filter
+              ? 'bg-primary-0 text-white-0 border-blue-3'
+              : 'bg-white-0 text-blue-1 border-gray-1 hover:bg-primary-2',
+          ]"
+          @click="timeFilter = filter"
+        >
+          {{ filter }}
+        </button>
+      </div>
+      <apexchart type="bar" height="400" :options="chartOptions" :series="series" />
+    </div>
+
+    <!-- 카테고리별 티켓 수-->
+    <div class="p-5 bg-white-0">
+      <h2 class="font-bold mb-5">카테고리별 티켓 수</h2>
+      <apexchart type="bar" height="400" :options="chartOptions2" :series="series2" />
+    </div>
+  </section>
+</template>
+
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { timeFilter, chartData, updateChartData } from '../StatisticsTest.ts';
 
-// ApexChart 옵션 및 데이터 설정
 const series = ref([]);
 const chartOptions = ref({
   chart: {
@@ -26,18 +56,18 @@ const chartOptions = ref({
     },
   },
   xaxis: {
-    categories: [], // 담당자 이름 리스트
+    categories: [],
   },
   yaxis: {
     max: 100, // y축 최대값을 100으로 제한
   },
-  colors: ['#3570FF', '#828DCA'], // 진행중(In Progress), 완료(Closed) 색상
+  colors: ['#3570FF', '#828DCA'],
   legend: {
     position: 'top',
     horizontalAlign: 'right',
     markers: {
-      radius: 12, // 마커의 크기
-      shape: 'circle', // 마커를 원형으로 설정
+      radius: 12,
+      shape: 'circle',
     },
   },
 });
@@ -73,31 +103,74 @@ watch(
 // 초기 데이터 처리
 updateChartData();
 processChartData();
+
+const categories = ref([
+  '인프라검토',
+  'VM',
+  'NFS',
+  'DNS',
+  'SSL',
+  'LB',
+  '프로젝트 사용자',
+  'KE',
+  'CR',
+  '솔루션',
+  '기타',
+]);
+
+// 카테고리가 추가되었을 때 아래의 색상을 반복함
+const customColors = [
+  '#232D64',
+  '#1F295B',
+  '#2A3570',
+  '#1B2350',
+  '#2E3B75',
+  '#19204C',
+  '#36408A',
+  '#1C285D',
+  '#404895',
+  '#121738',
+  '#4C5CB2',
+];
+
+const series2 = ref([
+  {
+    name: '티켓 수',
+    data: [20, 17, 26, 18, 21, 19, 10, 8, 10, 12, 29],
+  },
+]);
+const chartOptions2 = ref({
+  chart: {
+    type: 'bar',
+    stacked: false,
+    toolbar: {
+      show: false,
+    },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '30%',
+      borderRadius: 5,
+      colors: {
+        ranges: [], // customColors를 데이터 포인트별로 지정 가능
+      },
+      distributed: true, // 데이터 포인트별로 색상 적용
+    },
+  },
+  dataLabels: {
+    enabled: true,
+    style: {
+      fontSize: '13px',
+    },
+  },
+  xaxis: {
+    categories: categories.value,
+  },
+  yaxis: {},
+  colors: customColors,
+  legend: {
+    show: false,
+  },
+});
 </script>
-
-<template>
-  <section class="overflow-x-auto mt-5 p-5 pb-20">
-    <!-- 담당자별 티켓 진행 상황 -->
-    <div class="p-5 bg-white-0">
-      <h2 class="font-bold mb-5">담당자별 티켓 진행 현황</h2>
-      <div class="flex gap-4">
-        <button
-          v-for="filter in ['WEEK', 'MONTH', 'QUARTER']"
-          :key="filter"
-          :class="[
-            'px-4 py-2 rounded-lg border text-sm',
-            timeFilter === filter
-              ? 'bg-primary-0 text-white-0 border-blue-3'
-              : 'bg-white-0 text-blue-1 border-gray-1 hover:bg-primary-2',
-          ]"
-          @click="timeFilter = filter"
-        >
-          {{ filter }}
-        </button>
-      </div>
-      <apexchart type="bar" height="400" :options="chartOptions" :series="series" />
-    </div>
-  </section>
-</template>
-
-<style scoped></style>
