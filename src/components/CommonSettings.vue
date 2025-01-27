c67c6ae1b74003fa3e50c516bb4f56755feaac1f
kimgorok <kjyse2@naver.com>
2025-01-28 01:29:56 +0900

    ♻refactor: PasswordInput 리팩토링

--- src/components/CommonSettings.vue
+++ src/components/CommonSettings.vue
@@ -4,8 +4,7 @@ import { useMemberStore } from '@/stores/memberStore';
 import { schema } from '@/utils/passwordSchema';
 import { useField, useForm } from 'vee-validate';
 import { ref } from 'vue';
-import SvgIcon from './common/SvgIcon.vue';
-import { EyeIcon, EyeSlashIcon } from '@/assets/icons/path';
+import PasswordInput from './common/PasswordInput.vue';
 
 const assignmentNotification = ref(false);
 const statusNotification = ref(false);
@@ -18,9 +17,9 @@ const { handleSubmit, errors, meta } = useForm({
   validationSchema: schema,
 });
 
-const { value: originalpwd } = useField('originalPwd');
-const { value: newPwd } = useField('newPwd');
-const { value: checkPwd } = useField('checkPwd');
+const { value: originalpwd } = useField<string>('originalPwd');
+const { value: newPwd } = useField<string>('newPwd');
+const { value: checkPwd } = useField<string>('checkPwd');
 
 const resetForm = () => {
   originalpwd.value = '';
@@ -73,25 +72,13 @@ const handleImageChange = async (event: Event) => {
     }
   }
 };
-
-// 비밀번호 표시 여부
-const showPwd = ref({
-  original: false,
-  new: false,
-  check: false,
-});
-
-// 비밀번호 표시/숨김 토글 함수
-const togglePwdVisibility = (field: 'original' | 'new' | 'check') => {
-  showPwd.value[field] = !showPwd.value[field];
-};
 </script>
 
 <template>
   <div class="flex-stack w-1/2">
     <section class="flex gap-8 items-center">
       <div class="relative">
-        <img :src="previewImage" class="border w-36 h-36 rounded-full bg-pink-300 object-cover" />
+        <img :src="previewImage" class="border w-36 h-36 rounded-full object-cover" />
         <label
           class="absolute bottom-1 right-2 w-7 h-7 bg-gray-2 rounded-full flex-center text-xl hover:bg-gray-1 cursor-pointer"
         >
@@ -106,89 +93,31 @@ const togglePwdVisibility = (field: 'original' | 'new' | 'check') => {
     </section>
 
     <form class="flex-stack gap-10 mt-14" @submit="onSubmit">
-      <div class="flex-stack gap-0.5 relative">
-        <label class="text-gray-0">현재 비밀번호</label>
-        <div class="relative">
-          <input
-            v-model="originalpwd"
-            class="w-full bg-gray-2 bg-opacity-80 rounded-lg py-2 px-3.5 focus:outline-none pr-10"
-            :type="showPwd.original ? 'text' : 'password'"
-            placeholder="현재 비밀번호를 입력해주세요"
-          />
-          <SvgIcon
-            class="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
-            @click="togglePwdVisibility('original')"
-            :icon="showPwd.original ? EyeIcon : EyeSlashIcon"
-          />
-        </div>
-      </div>
+      <PasswordInput
+        v-model="originalpwd"
+        label="현재 비밀번호"
+        placeholder="현재 비밀번호를 입력해주세요"
+        :error="errors.originalpwd"
+      />
 
-      <div class="flex-stack gap-0.5 relative">
-        <label class="text-gray-0">새 비밀번호</label>
-        <div class="relative">
-          <input
-            v-model="newPwd"
-            name="newPwd"
-            maxlength="20"
-            class="w-full bg-gray-2 bg-opacity-80 rounded-lg py-2 px-3.5 focus:outline-none"
-            :type="showPwd.new ? 'text' : 'password'"
-            placeholder="새 비밀번호를 입력해주세요"
-          />
-          <SvgIcon
-            class="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
-            @click="togglePwdVisibility('new')"
-            :icon="showPwd.new ? EyeIcon : EyeSlashIcon"
-          />
-        </div>
-        <p
-          :class="[
-            'absolute top-[70px]',
-            {
-              'text-gray-1': !newPwd,
-              'text-red-1': errors.newPwd,
-              'text-green-1': newPwd && !errors.newPwd,
-            },
-          ]"
-        >
-          {{
-            errors.newPwd
-              ? errors.newPwd
-              : newPwd
-              ? '사용 가능한 비밀번호입니다.'
-              : '비밀번호는 영문, 숫자, 특수문자를 포함한 8자리 이상 20자리 이하'
-          }}
-        </p>
-      </div>
+      <PasswordInput
+        v-model="newPwd"
+        label="새 비밀번호"
+        name="newPwd"
+        maxlength="20"
+        placeholder="새 비밀번호를 입력해주세요"
+        :error="errors.newPwd"
+        :success-message="newPwd && !errors.newPwd ? '사용 가능한 비밀번호입니다.' : ''"
+      />
 
-      <div class="flex-stack gap-0.5 relative">
-        <label class="text-gray-0">새 비밀번호 확인</label>
-        <div class="relative">
-          <input
-            v-model="checkPwd"
-            name="checkPwd"
-            class="w-full bg-gray-2 bg-opacity-80 rounded-lg py-2 px-3.5 focus:outline-none"
-            :type="showPwd.check ? 'text' : 'password'"
-            placeholder="새 비밀번호를 확인해주세요"
-          />
-          <SvgIcon
-            class="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
-            @click="togglePwdVisibility('check')"
-            :icon="showPwd.check ? EyeIcon : EyeSlashIcon"
-          />
-        </div>
-        <p
-          v-if="checkPwd"
-          :class="[
-            'absolute top-[70px]',
-            {
-              'text-red-1': errors.checkPwd,
-              'text-green-1': !errors.checkPwd && checkPwd,
-            },
-          ]"
-        >
-          {{ errors.checkPwd || '비밀번호가 일치합니다.' }}
-        </p>
-      </div>
+      <PasswordInput
+        v-model="checkPwd"
+        label="새 비밀번호 확인"
+        name="checkPwd"
+        placeholder="새 비밀번호를 확인해주세요"
+        :error="errors.checkPwd"
+        :success-message="checkPwd && !errors.checkPwd ? '비밀번호가 일치합니다.' : ''"
+      />
 
       <!-- 알림 조건부 렌더링 -->
       <div class="flex-stack gap-5 mt-5">
