204ea45c2c374ee8cf775c80afdde72ba02e4058
Pseokjong <seogjongb@gamil.com>
2025-02-01 00:40:29 +0900

    ✨feat:[관리자] 관리자 메인 페이지 레이아웃 제작 Related to: #21

---
+++ src/views/Administrator/AdminMainView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex gap-5 p-5 h-screen">
+    <AdminMainStatistics class="w-4/5 h-full overflow-y-auto hide-scrollbar" />
+    <AdminTotalUserList class="w-1/5 h-full sticky top-0" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AdminMainStatistics from '@/components/Administrator/AdminMain/AdminMainStatistics.vue';
+import AdminTotalUserList from '@/components/Administrator/AdminMain/AdminTotalUserList.vue';
+</script>
+
+<style scoped></style>
