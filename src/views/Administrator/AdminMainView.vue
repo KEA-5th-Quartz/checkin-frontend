995ee3abd91747648e66136bcea969555a078ae0
Pseokjong <seogjongb@gamil.com>
2025-02-01 01:32:15 +0900

    🐛fix:[관리자] 관리자 메인 페이지 레이아웃 넓이 적용 수정 Related to: #21

--- src/views/Administrator/AdminMainView.vue
+++ src/views/Administrator/AdminMainView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex gap-5 p-5 h-screen">
-    <AdminMainStatistics class="w-4/5 h-full overflow-y-auto hide-scrollbar" />
-    <AdminTotalUserList class="w-1/5 h-full sticky top-0" />
+    <AdminMainStatistics class="flex-[4] h-full overflow-y-auto hide-scrollbar" />
+    <AdminTotalUserList class="flex-[1] h-full sticky top-0" />
   </div>
 </template>
 
