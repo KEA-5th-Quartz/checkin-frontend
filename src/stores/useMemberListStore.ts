5f274ad5a135aadb711fb03fc47a61e11bc9246d
Pseokjong <seogjongb@gamil.com>
2025-02-02 17:43:53 +0900

    ✨feat:[관리자] 멤버 목록 관리용 Pinia 스토어 추가 Related to: #31

---
+++ src/stores/useMemberListStore.ts
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia';
+import { MemberListItem } from '@/types/memberList';
+
+export const useMemberListStore = defineStore('member-list', {
+  state: () => ({
+    members: [] as MemberListItem[], // 멤버 목록
+    currentPage: 1, // 현재 페이지
+    totalPages: 0, // 총 페이지 수
+    selectedRole: 'USER' as MemberListItem['role'], // 선택된 역할
+    searchQuery: '', // 검색어
+  }),
+  actions: {
+    setMembers(members: MemberListItem[], totalPages: number) {
+      this.members = members;
+      this.totalPages = totalPages;
+    },
+    setRole(role: MemberListItem['role']) {
+      this.selectedRole = role;
+    },
+    setSearchQuery(query: string) {
+      this.searchQuery = query;
+    },
+    setCurrentPage(page: number) {
+      this.currentPage = page;
+    },
+  },
+});
