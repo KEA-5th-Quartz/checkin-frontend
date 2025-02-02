01ad21996e5d056077d1a61597304038f9171ef0
viola30th <viola30th@gmail.com>
2025-02-02 16:29:16 +0900

    ♻️refactor:[사용자] 제목,설명 입력 제약조건 수정
    Related to: #18

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -5,7 +5,6 @@ export const validationSchema = yup.object({
     .string()
     .trim()
     .required('* 제목을 입력하세요.')
-    .min(20, '* 제목은 최소 20자 이상이어야 합니다.')
     .max(25, '* 제목은 최대 25자까지 입력 가능합니다.')
     .matches(/^[^\s]+(\s+[^\s]+)*$/, '* 공백만 입력할 수 없습니다.')
     .matches(/^[\p{L}\p{N}\p{P}\p{Z}]+$/u, '* 특수문자는 사용할 수 없습니다.'),
@@ -34,7 +33,7 @@ export const validationSchema = yup.object({
     .string()
     .trim()
     .required('* 설명을 입력하세요.')
-    .min(30, '* 설명은 최소 30자 이상이어야 합니다.')
+    .min(10, '* 설명은 최소 10자 이상이어야 합니다.')
     .max(100, '* 설명은 최대 100자까지 입력 가능합니다.')
     .matches(/^[^\s]+(\s+[^\s]+)*$/, '* 공백만 입력할 수 없습니다.')
     .matches(/^(?!.*<script>).*$/, '* HTML 태그를 포함할 수 없습니다.'),
