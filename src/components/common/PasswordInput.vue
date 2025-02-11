e660ec0d46f48527aa1b069a91e070c9f3e9a157
kimgorok <kjyse2@naver.com>
2025-02-11 21:46:29 +0900

    🐛fix: 비밀번호에 이모지와 공백 넣지 못하게 정규식 추가 Related to: #77

--- src/components/common/PasswordInput.vue
+++ src/components/common/PasswordInput.vue
@@ -11,6 +11,7 @@ withDefaults(defineProps<PasswordInputProps>(), {
   maxlength: undefined,
   error: '',
   successMessage: '',
+  modelValue: '',
 });
 
 const emit = defineEmits<{
@@ -22,6 +23,17 @@ const showPassword = ref(false);
 const toggleVisibility = () => {
   showPassword.value = !showPassword.value;
 };
+
+const handleInput = (e: Event) => {
+  const input = (e.target as HTMLInputElement).value;
+  const cleanedValue = input
+    .replace(
+      /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu,
+      '',
+    )
+    .replace(/\s/g, '');
+  emit('update:modelValue', cleanedValue);
+};
 </script>
 
 <template>
@@ -30,7 +42,7 @@ const toggleVisibility = () => {
     <div class="relative">
       <CommonInput
         :value="modelValue"
-        @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        @input="handleInput"
         class="w-full bg-gray-2 bg-opacity-40 rounded-lg py-2 px-3.5 focus:outline-none pr-10 placeholder:text-sm"
         :type="showPassword ? 'text' : 'password'"
         :placeholder="placeholder"
