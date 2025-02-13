2ebc35f407a873e72ec340defb78bd0f82913484
kimgorok <kjyse2@naver.com>
2025-02-13 11:14:13 +0900

    🐛fix: 모든 설명이라는 텍스트를 요청사항으로 변경 Related to: #74

--- src/utils/ticketEditValidation.ts
+++ src/utils/ticketEditValidation.ts
@@ -18,9 +18,9 @@ export const ticketEditValidationSchema = yup.object({
   content: yup
     .string()
     .trim()
-    .required('* 설명을 입력하세요')
-    .min(10, '* 설명은 최소 10자 이상이어야 합니다')
-    .max(100, '* 설명은 최대 100자까지 입력 가능합니다')
+    .required('* 요청사항을 입력하세요')
+    .min(10, '* 요청사항은 최소 10자 이상이어야 합니다')
+    .max(100, '* 요청사항은 최대 100자까지 입력 가능합니다')
     .matches(/^[^\s]+(\s+[^\s]+)*$/, '* 공백만 입력할 수 없습니다')
     .matches(/^(?!.*<script>).*$/, '* HTML 태그를 포함할 수 없습니다'),
 
